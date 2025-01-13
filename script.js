4604322779f82ae5e16d4276f7fb8dbc83947ac2
Ajjayyaswamy <144276625+Ajjayyaswamy@users.noreply.github.com>
2025-01-14 04:45:23 +0530

    aj-portfolio initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".text", {
+  strings: [
+    "Full Stack Developer",
+    "Angular Developer",
+    "Java Developer",
+    "Web Developer",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 100,
+  loop: true,
+});
