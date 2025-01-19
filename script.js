30f532dae356c270748fcc8f76879fc0642b718e
Ajjayyaswamy <144276625+Ajjayyaswamy@users.noreply.github.com>
2025-01-19 15:40:15 +0530

    Changes-02

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ var typed = new Typed(".text", {
   backDelay: 100,
   loop: true,
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.getElementById("menu-icon");
+  const navbar = document.querySelector(".navbar");
+  const topIcon = document.querySelector(".top");
+
+  menuIcon.addEventListener("click", function () {
+    navbar.classList.toggle("active");
+  });
+
+  topIcon.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.getElementById("home").scrollIntoView({ behavior: "smooth" });
+  });
+});
